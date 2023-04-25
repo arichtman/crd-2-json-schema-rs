a1165b21ebd0a02731b126a65769eb4486d0e622
Richtman, Ariel <10679234+arichtman@users.noreply.github.com>
2023-04-25 20:45:31 +1000

    wip, slowly making progress

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@
 use openapiv3::OpenAPI;
 use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
+use std::collections::HashMap;
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct Names {
     plural: String,
     singular: String,
@@ -20,7 +21,7 @@ struct Names {
     short_names: Vec<String>,
 }
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct Version {
     name: String,
     served: bool,
@@ -28,7 +29,7 @@ struct Version {
     schema: OpenAPI,
 }
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct Spec {
     group: String,
     versions: Vec<Version>,
@@ -36,19 +37,26 @@ struct Spec {
     names: Names,
 }
 
-#[derive(Debug)] //, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct KeyValuePair {
+    name: String,
+    value: String,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct Crd {
-    api_version: String,
+    apiVersion: String,
     kind: String,
-    metadata: Vec<(String, String)>,
-    spec: Spec,
+    metadata: HashMap<String, String>,
+    // metadata: Vec<KeyValuePair>,
+    // spec: Spec,
 }
 
 fn main() {
     //TODO: should we use if let here?
     if let Ok(file) = fs::File::open(String::from("resource-definition.yaml")) {
-        let data_structure: OpenAPI = serde_yaml::from_reader(file).unwrap();
-        // let data_structure: Crd = serde_yaml::from_reader(file).unwrap();
+        // let data_structure: OpenAPI = serde_yaml::from_reader(file).unwrap();
+        let data_structure: Crd = serde_yaml::from_reader(file).unwrap();
         // if let Ok(file_contents) = fs::read_to_string(String::from("resource-definition.yaml")) {
         // let data_structure = serde_yaml::from_str(&file_contents);
         println!("{:?}", data_structure);
