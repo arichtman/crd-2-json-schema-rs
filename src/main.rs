2112c5b0c326906769b1e2c98497bde897c57890
Richtman, Ariel <10679234+arichtman@users.noreply.github.com>
2023-05-01 20:46:00 +1000

    added some spec checking

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@
 
 use serde::{Deserialize, Serialize};
 use serde_yaml;
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fs;
 
@@ -68,11 +69,29 @@ struct Crd {
 fn main() {
     //TODO: should we use if let here?
     if let Ok(file) = fs::File::open(String::from("resource-definition.yaml")) {
-        let data_structure: Crd = serde_yaml::from_reader(file).unwrap();
-        println!("{:?}", data_structure);
-        if let Some(crd_name) = data_structure.metadata.get("name") {
+        let crd: Crd = serde_yaml::from_reader(file).unwrap();
+        println!("{:?}", crd);
+        let crd_spec: Vec<Version> = crd
+            .spec
+            .versions
+            .into_iter()
+            .filter(|x| x.storage)
+            .collect();
+        //TODO: would it be better here to only check success condition with an if
+        //  and throw a generic panic for all non-1 cases?
+        match crd_spec.len().cmp(&1) {
+            //TODO: These side effects kinda smell....
+            //TODO: Preeeetty sure panicking is lousy for testing, even then the text coupling is poor.
+            Ordering::Less => panic!("No specs found marked as storage"),
+            Ordering::Equal => (),
+            Ordering::Greater => {
+                panic!("One and only one version must be marked as the storage version")
+            }
+        };
+        if let Some(crd_name) = crd.metadata.get("name") {
             println!("{}", crd_name);
         };
+
         todo!("convert data structure to schema");
     }
 }
