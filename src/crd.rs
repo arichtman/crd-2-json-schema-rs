43be55d5925717a13ead1d3e0042bd16d484367b
Ariel Richtman <Ariel.Richtman@SilverRailTech.com>
2023-06-24 19:40:33 +1000

    progressed the output

--- src/crd.rs
+++ src/crd.rs
@@ -72,7 +72,6 @@ impl Crd {
             .filter(|x| x.storage)
             //TODO: We can possibly use take(1) here and return immediately
             .collect();
-        //TODO: Is there any point using match here?
         if crd_specs.len() == 1 {
             //TODO: Find out why we have to clone twice
             Some(crd_specs[0].clone())
@@ -80,10 +79,10 @@ impl Crd {
             None
         }
     }
-    //TODO: Use static lifetime or just clone?
     pub fn get_name(&self) -> Option<&String> {
         self.metadata.get("name")
     }
+    //TODO: Use static lifetime or just clone?
     pub fn get_spec_group(&self) -> String {
         self.spec.group.clone()
     }
@@ -97,87 +96,56 @@ impl Crd {
 
 #[derive(Serialize, Deserialize)]
 pub struct CRDJsonSchema(serde_json::Value);
-// everything: serde_json::Value, // everything: HashMap<String, serde_json::Value>, // serde_json::Value
-// const schema: String = "http://json-schema.org/draft-07/schema#",
-// title: String,
-// schema_version: String,
-// kind: String,
-// schema: HashMap<String, OpenAPI>,
-// }
-
-// impl CRDJsonSchema {
-//     fn get_description(&self) -> String {
-//         //TODO: We're not actually storing the group granularly?
-//         format!(
-//             "Generated JSON schema for {}'s {} CRD",
-//             self.schema_version, self.schema_version
-//         )
-//     }
-// }
 
 impl From<Crd> for CRDJsonSchema {
     fn from(crd: Crd) -> CRDJsonSchema {
+        // TODO: Should this error be handled here or do we consider a CRD invalid if no versions stored?
         // let spec = crd
         // .get_current_version()
         // .expect("CRD versions must contain at least one marked for storage");
         let version = crd.get_current_version().unwrap();
-        CRDJsonSchema(json!({"schema": "http://json-schema.org/draft-07/schema#",
-            "title": crd.get_name(),
-            // TODO: work out version display normally
-            "description": format!("Generated JSON schema for {:?}'s {:?} CRD", version, version)}))
-        // CRDJsonSchema {
-        //     //TODO: See about ownership and references here
-        //     title: crd
-        //         .get_name()
-        //         .expect("CRD name must be present")
-        //         .to_string(),
-        //     schema_version: format!("{}/{}", crd.get_spec_group(), spec.name),
-        //     kind: crd.get_spec_names_kind(),
-        //     schema: spec.schema,
-        // }
+        CRDJsonSchema(json!({
+        "schema": "http://json-schema.org/draft-07/schema#",
+        "title": crd.get_name(),
+        // TODO: work out version display normally
+        "description": format!("Generated JSON schema for {:?}'s {:?} CRD", version, version),
+        "type": "object",
+        "properties": {
+            "apiVersion": {
+                "type": "string",
+                "enum": version,
+                "description": "a string that identifies the version of the schema the object should have. For CRDs this is the crdGroup/version"
+            },
+            "kind": {
+                    "type": "string",
+                    "const": crd.kind,
+                    "description": "a string the identifies the kind of resource that this document represents"
+                },
+            "metadata": {
+                "required": [ "name" ],
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string",
+                        "description": "a string that uniquely identifies this object within the current namespace"
+                    },
+                    "labels": {
+                        "type": "object",
+                        "description": "a map of string keys and values that can be used to organize and categorize objects, for more details see https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/"
+                    },
+                    "annotations": {
+                        "type": "object",
+                        "description": "a map of string keys and values that can be used by external tooling to store and retrieve arbitrary metadata about this object, for more details see https://kubernetes.io/docs/concepts/overview/working-with-objects/annotations/"
+                    }
+                }
+            }
+        },
+        "spec": {},
+        "required": [ "apiVersion", "kind", "metadata", "spec" ],
+        // TODO: This wacky dynamic bit, somehow
+        // "allOf":
+        }))
     }
 }
-
-// impl fmt::Display for CRDJsonSchema {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         //TODO: Printing logic
-//         write!(f, "{{\"$schema\": \"http://json-schema.org/draft-07/schema#\",
-//             \"title\": \"{}\",
-//             \"description\": \"{}\",
-//             \"type\": \"object\",
-//             \"properties\": {{
-//                 \"apiVersion\": {{
-//                     \"type\": \"string\",
-//                     \"enum\": \"{}\",
-//                     \"description\": \"a string that identifies the version of the schema the object should have. For CRDs this is the crdGroup/version\"
-//                 }},
-//                 \"kind\": {{
-//                         \"type\": \"string\",
-//                         \"const\": \"{}\",
-//                         \"description\": \"a string the identifies the kind of resource that this document represents\"
-//                     }},
-//                 \"metadata\": {{
-//                     \"type\": \"object\",
-//                     \"properties\": {{
-//                         \"name\": {{
-//                             \"type\": \"string\",
-//                             \"description\": \"a string that uniquely identifies this object within the current namespace\"
-//                         }},
-//                         \"labels\": {{
-//                             \"type\": \"object\",
-//                             \"description\": \"a map of string keys and values that can be used to organize and categorize objects, for more details see https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/\"
-//                         }},
-//                         \"annotations\": {{
-//                             \"type\": \"object\",
-//                             \"description\": \"a map of string keys and values that can be used by external tooling to store and retrieve arbitrary metadata about this object, for more details see https://kubernetes.io/docs/concepts/overview/working-with-objects/annotations/\"
-//                         }}
-//                     }},
-//                 \"required\": [\"name\"]
-//                 }},
-//             \"spec\": {{}}
-//             }},
-//             \"required\": [\"apiVersion\", \"kind\", \"metadata\", \"spec\"],
 //             \"allOf\": [ {{ \"if\": {{ \"properties\": {{ \"apiVersion\": {{ \"const\": \"{}\" }}}}}}}}]
 //             }}", self.title, self.get_description(), self.schema_version, self.kind, self.schema_version)
-//     }
-// }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,7 @@
     unreachable_code,
     non_snake_case
 )]
-//TODO: When to use which of these?
-//TODO: Should we be making these public?
-//  I figured they were internal use only by main.rs
+// TODO: Learn about module structuring
 mod crd;
 use crd::{CRDJsonSchema, Crd, Version};
 use serde_yaml;
@@ -20,14 +18,7 @@ fn main() {
     let file =
         fs::File::open(String::from("resource-definition.yaml")).expect("Error opening file");
     let crd: Crd = serde_yaml::from_reader(file).unwrap();
-    // println!("{:?}", crd);
-    let crd_spec = crd.get_current_version().unwrap();
-    //TODO: Same question for testing...
-    let crd_name = crd.get_name().expect("CRD must have a name");
-    // println!("{}", crd_name);
-    let crd_group = crd.get_spec_group();
-    let crd_spec_name = crd.get_spec_name();
-    let crd_spec_kind = crd.get_spec_names_kind();
+    crd.get_name().expect("CRD must have a name");
     // So we want a write out
     // I don't think that needs more than an implementation of Display
     // However we want maybe a struct that represents the output jsonschema
